eeca0dba47dd6924d9ddc442843fe962e7db051b
victoriabernard92 <Bernard.Victoria22@gmail.com>
2016-03-08 16:16:50 -0800

    Answers for exercise 3

--- lib/receipt_printer.rb
+++ lib/receipt_printer.rb
@@ -13,20 +13,29 @@ class ReceiptPrinter
   end
 
   def print
-    subtotal = items.reduce(0) do |sum, item|
+    subtotal = items.reduce(0) do |sum, item| #|sum, item| is the elements we can no DO stuff with
       item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
-
-      sum + item_cost.to_i
+    #  output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
+      print_item(item, item_cost)
+      sum + item_cost.to_i # converts to INT and adds to subtotal
     end
 
     output.puts divider
-    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
+  #  output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
+    print_subtot(subtotal)
     output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
     output.puts divider
     output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
   end
+  def print_subtot(subtotal)
+    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
+  end
+  def calc_subtotal()
 
+  end
+  def print_item(item, item_cost)
+      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
+  end
   private
 
   attr_reader :output, :items
